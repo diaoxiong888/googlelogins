ecc18ee7f419cac2f466158d99ce457db56e4bae
xin <lx-lx@yeah.net>
2015-06-05 11:37:45 -0400

    1

---
+++ www/googleforgiv.js
@@ -0,0 +1,29 @@
+//import exec module
+var exec = require('cordova/exec');
+
+//define googleforgiv object
+var googleforgiv = {
+    //define method
+    isAvailable: function (successCallback) {
+        exec(successCallback, null, 'googleforgiv', 'isAvailable', []);
+    },
+
+    login: function (options, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'googleforgiv', 'login', [options]);
+    },
+
+    trySilentLogin: function (options, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'googleforgiv', 'trySilentLogin', [options]);
+    },
+
+    logout: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'googleforgiv', 'logout', []);
+    },
+
+    disconnect: function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, 'googleforgiv', 'disconnect', []);
+    }
+};
+
+//export module googleforgiv
+module.exports = googleforgiv;
